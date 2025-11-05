5a85ac7689cfec498512c8bbda8d73e6f30a5e14
johntmunger <johntmunger@gmail.com>
2025-11-05 12:57:16 -0700

    use store in nav for friend output length

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <script setup>
 import MyFriends from './components/MyFriends.vue';
+import { friendStore } from './stores/friends';
+
+const friends = friendStore();
 </script>
 
 <template>
-  <main class="">
+  <main>
+    <nav class="border">
+      <p class="text-right p-4">
+        You have
+        <span v-if="friends.names.length === 0">no friends</span>
+        <span v-else-if="friends.names.length === 1">{{ friends.names.length }} friend!</span>
+        <span v-else> {{ friends.names.length }} friends!</span>
+      </p>
+    </nav>
     <MyFriends />
   </main>
 </template>
